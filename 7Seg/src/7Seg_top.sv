03ee4b571cb1e17c2674dfdfb3d3443e37e622c4
Connor Coale <connorcoale@gmail.com>
2023-02-08 11:19:10 -0500

    initial commit

---
+++ 7Seg/src/7Seg_top.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/07/2023 11:48:31 AM
+// Design Name: 
+// Module Name: 7Seg_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module 7Seg_top(
+
+    );
+endmodule
