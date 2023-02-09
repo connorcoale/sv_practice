dd34131829ec075c86694781bbd07a922b6051ad
Connor Coale <connorcoale@gmail.com>
2023-02-09 13:01:38 -0500

    created functionality to test SevSeg module

--- SevSeg/src/SevSeg_top.sv
+++ SevSeg/src/SevSeg_top.sv
@@ -21,6 +21,19 @@
 
 
 module SevSeg_top(
-
+    input  logic CLK100MHZ,
+    input  logic [3:0] sw,
+    input  logic [1:0] btn,
+    output logic [7:0] ja
     );
+    
+    logic [3:0] dig0;
+    logic [3:0] dig1;
+    
+    SevSeg display1(.clk(CLK100MHZ), .dig0(dig0), .dig1(dig1), .cat(ja[6:0]), .an(ja[7]));
+    
+    always_ff @(posedge CLK100MHZ) begin
+        dig0 <= btn[0] ? sw : dig0; // I think the buttons are active low
+        dig1 <= btn[1] ? sw : dig1;
+    end
 endmodule
