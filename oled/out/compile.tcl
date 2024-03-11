8e901a013e3efb30d7cc718f7f1d6bf46204413c
Connor Coale <connorcoale@gmail.com>
2024-03-10 23:56:05 -0400

    added some helper tcl scripts for use with Vivado tcl mode (or batch mode)

---
+++ oled/out/compile.tcl
@@ -0,0 +1,11 @@
+read_verilog -sv ../src/oled_top.sv ../../spi/src/spi_master.sv
+
+read_xdc ../xdc/Arty-A7-35.xdc
+
+synth_design -top oled_top -part xc7a35ticsg324-1L
+
+opt_design
+place_design
+route_design
+
+write_bitstream -force oled.bit
---
+++ oled/out/reflash.tcl
@@ -0,0 +1,2 @@
+set_property PROGRAM.FILE oled.bit [current_hw_device]
+program_hw_devices [current_hw_device]
