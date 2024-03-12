edfe6182b8874ec4a3bcba6a98203017f20e39c2
Connor Coale <connorcoale@gmail.com>
2024-03-11 23:47:44 -0400

    Working test pattern upon btn2 press

--- oled/src/oled_top.sv
+++ oled/src/oled_top.sv
@@ -15,6 +15,7 @@ module oled_top #(
                    input logic  clk,
                    input logic  reset,
                    input logic  reset_display_raw,
+                   input logic  btn2_raw,
 
                    output logic [7:0] led,
 
@@ -30,27 +31,44 @@ module oled_top #(
                    output logic sclk
                  );
    localparam logic [7:0]       DisplayOn = 8'hAF;
+`ifdef VERILATOR
+   localparam Delay3v3          = 2_000;  // 20us at 100MHz
+   localparam StartupDCDelay    = 1_500;       // 15us  .
+   localparam StartupResetDelay = 1_500;       // 15us  .
+   localparam ComSegDelay       = 10_000; // 100us .
+   localparam integer           DebounceCount = 100; // us at 10MHz
+`else
    localparam Delay3v3          = 2_000_000;  // 20ms at 100MHz
    localparam StartupDCDelay    = 1500;       // 15us  .
    localparam StartupResetDelay = 1500;       // 15us  .
    localparam ComSegDelay       = 10_000_000; // 100ms .
+   localparam integer           DebounceCount = 100_000; // 1ms at 10MHz
+`endif
 
    // Need to debounce and monopulse the reset_display input.
    logic              reset_display, reset_display_db;
-   localparam integer DebounceCount = 100_000; // 1ms at 10MHz
    debouncer  #(.N(DebounceCount)) debouncer
                          (
                           .out       (reset_display_db),
                           .clk       (clk),
                           .reset     (reset),
                           .in        (reset_display_raw)
-                         );
+                          );
    monopulser monopulser (
                           .clk       (clk),
                           .reset     (reset),
                           .in        (reset_display_db),
                           .out       (reset_display)
                           );
+
+   logic              btn2;
+   debouncer  #(.N(DebounceCount)) debouncer2
+                         (
+                          .out       (btn2),
+                          .clk       (clk),
+                          .reset     (reset),
+                          .in        (btn2_raw)
+                         );
 
    logic ready, done, start;
    logic [PACKET_WIDTH-1:0] d_in;
@@ -83,8 +101,11 @@ module oled_top #(
                  STARTUP_RESET_DELAY, // 3 15us settle time for after reset goes back high
                  DISP_ON,             // 4 issue command to turn display on
                  COM_SEG_DELAY,       // 5 100ms to let com/seg come up
-                 SEND_COMMAND,        // 6
-                 SEND_DATA            // 7
+                 SET_256_MODE1,       // 6
+                 SET_256_MODE2,       // 7
+                 SEND_COLORS,         // 8
+                 SEND_COMMAND,        // 9
+                 SEND_DATA            // 10
                  } state_t;
 
    always_ff @(posedge clk or posedge reset) begin
@@ -98,6 +119,7 @@ module oled_top #(
          vcc_en      <= 1'b0;
          pmod_en     <= 1'b0;
          cs          <= 1'b1;
+         count       <= 0;
       end else begin
          state       <= state_next;
          d_in        <= d_in_next;
@@ -108,6 +130,7 @@ module oled_top #(
          vcc_en      <= vcc_en_next;
          pmod_en     <= pmod_en_next;
          cs          <= cs_next;
+         count       <= count_next;
       end
    end
 
@@ -118,6 +141,7 @@ module oled_top #(
    logic                    send_command, send_data;
 
    logic [PACKET_WIDTH-1:0] d_in_next;
+   logic [7:0]              count, count_next;
 
    assign led = {4'b0000, state[3:0]};
 
@@ -133,6 +157,7 @@ module oled_top #(
       pmod_en_next = pmod_en;
       cs_next = 1'b1;
       n_delay = '0;
+      count_next = count;
       case (state)
         IDLE: begin
            if (reset_display) begin
@@ -141,6 +166,7 @@ module oled_top #(
            end
            else if (send_command) state_next = SEND_COMMAND;
            else if (send_data) state_next = SEND_DATA;
+           else if (btn2) state_next = SEND_COLORS;
         end
         STARTUP_3V3_DELAY: begin
            // Start with reset high (active low reset)
@@ -193,7 +219,33 @@ module oled_top #(
         COM_SEG_DELAY: begin
            // delay 100ms
            n_delay = ComSegDelay;
-           if (delay_done) state_next = IDLE;
+           if (delay_done) state_next = SET_256_MODE1;
+        end
+        SET_256_MODE1: begin
+           cs_next = 1'b0;
+           if (ready) begin
+              d_in_next = 8'hA0; // Send driver remap/color depth command
+              start_next = 1'b1;
+           end
+           if (done)state_next = SET_256_MODE2;
+        end
+        SET_256_MODE2: begin
+           cs_next = 1'b0;
+           if (ready) begin
+              d_in_next = 8'h00; // Set to 256 color format (bits 7 and 6 must be 0)
+              start_next = 1'b1;
+           end
+           if (done) state_next = IDLE;
+        end
+        SEND_COLORS: begin
+           cs_next = 1'b0;
+           dc_next = 1'b1;
+           count_next = count + done;
+           if (ready) begin
+              d_in_next = count; // send an incrementing counter as data
+              start_next = 1'b1;
+           end
+          if (done) state_next = btn2 ? SEND_COLORS : IDLE;
         end
         SEND_COMMAND: begin
            dc_next = 0;
